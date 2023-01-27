3c27d32bf8ff87de3e5c8fec4f27acbdb9065856
hsivov <hsivov@students.softuni.bg>
2023-01-27 23:01:35 +0200

    Add structure and styling

---
+++ projects/countdown-timer/README.md
@@ -0,0 +1,3 @@
+# Countdown Timer
+App counting down the time remaining until a certain point in the future. In this case until the start of World Cup 2022 in Qatar.
+> Intend to add functionality that user can choose point in time.
---
+++ projects/countdown-timer/script.js
@@ -0,0 +1,31 @@
+const eventDateString = "22 May 2023 10:00"
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function countdownTimer(){
+    const eventDate = new Date(eventDateString);
+    const currentDate = new Date();
+
+    const totalSeconds = (eventDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = formatOutput(minutes);
+    secondsEl.innerHTML = formatOutput(seconds);
+}
+
+function formatOutput(timer){
+    return timer < 10 ? `0${timer}`: timer;
+}
+
+countdownTimer();
+
+setInterval(countdownTimer, 1000);
